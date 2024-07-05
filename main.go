4fcc7d996d5467e40af56fe5b10e37b829022d08
epiharyono <epiharyono@gmail.com>
2024-07-05 16:21:19 +0700

    update

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ type JWTHeader struct {
 	Sub int64 `json:"sub"`
 }
 
-func GetID() {
+func GetID() int64 {
 	// Example JWT
 	jwt := "eyJhbGciOiJIUzI1NiIsImtpZCI6ImdseGJpSVVZTVhTM0ZPakxrMHNBZHRJWmZGYk9Zc3NaIiwidHlwIjoiSldUIn0.eyJleHAiOjE3MjA0MjIxMzcsInN1YiI6M30.PNLgPdkahPB649QTvCX_Fab9DewRA9xyx2Dv0RnMooY"
 
@@ -19,14 +19,14 @@ func GetID() {
 	parts := strings.Split(jwt, ".")
 	if len(parts) != 3 {
 		fmt.Println("Invalid JWT")
-		return
+		return 0
 	}
 
 	// Decode the header
 	headerBytes, err := base64.RawURLEncoding.DecodeString(parts[1])
 	if err != nil {
 		fmt.Println("Error decoding header:", err)
-		return
+		return 0
 	}
 
 	// Unmarshal the JSON header into a struct
@@ -34,10 +34,10 @@ func GetID() {
 	err = json.Unmarshal(headerBytes, &header)
 	if err != nil {
 		fmt.Println("Error unmarshalling header:", err)
-		return
+		return 0
 	}
 
 	// Print the decoded header
 	fmt.Printf("Decoded JWT Header: %+v\n", header.Sub)
-
+	return header.Sub
 }
--- say_hello.go
+++
@@ -1,5 +0,0 @@
-package go_jwt
-
-func SayHello() string {
-	return "Hello World "
-}
